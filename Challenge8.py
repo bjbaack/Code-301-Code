309ce57b480dc318d2319ce0e172308a0f03e0c1
bradbaack <bjbaack@gmail.com>
2024-04-03 15:16:21 -0500

    first commit

---
+++ Challenge8.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Script:                       Ops 301 Ops Chall #8
+# Author:                       Brad Baack
+# Date of latest revision:      04/3/2024
+# Purpose:                      Create a list
+
+# Assign a list of ten string elements to a variable
+items = ["item1", "item2", "item3", "item4", "item5", "item6", "item7", "item8", "item9", "item10"]
+
+# Print the fourth element of the list
+print("The fourth element is:", items[3])
+
+# Print the sixth through tenth elements of the list
+print("The sixth through tenth elements are:", items[5:])
+
+# Change the value of the seventh element to "onion"
+items[6] = "onion"
+
+# Print the modified list
+print("The modified list is:", items)
